05a77dc8d79a1b009d908156d16d8c09a8dce2a9
Suraj Bhardwaj <suraj.bhardwaj@ad.mindbowser.com>
2026-04-15 11:58:31 +0530

    config step update

--- Api_PWAutomation/playwright.config.js
+++ Api_PWAutomation/playwright.config.js
@@ -1,32 +1,22 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+const { defineConfig, devices } = require('@playwright/test');
 
-export default defineConfig({
+module.exports = defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
+  workers: 1,
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['dot'],
     ['html', { open: 'never' }]
   ],
   use: {
-    screenshot: 'only-on-failure',
-    trace: 'on-first-retry'
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
   },
-
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
   ],
 });
-
